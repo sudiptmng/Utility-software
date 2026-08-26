4ca23f2d9bc9752d88e7208efef2be8567c0d856
sudip <sktamangpersonal@gmail.com>
2026-08-26 17:04:53 +0545

    extend JWT access token lifetime to 2 hours

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +152,8 @@ FERNET_KEY = b'oML8mCRre1pzvrUCrsEOBKD6IrxGKOI9xRJUHNdrIsM='
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
