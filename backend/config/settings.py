03ad6df94deba5fff9fd798bc52076483d2e8dd7
Suresh Khadka <khadkasuresh647@gmail.com>
2026-08-26 22:15:43 +0545

    add docker part

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -24,4 +24,13 @@ api.interceptors.response.use(
   },
 );
 
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
