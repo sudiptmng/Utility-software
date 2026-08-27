b2cf8da4b6e9902fc6f097bdd15afab2dea66e9e
sudip <sktamangpersonal@gmail.com>
2026-08-27 06:59:36 +0545

    update axios config for docker-compose and clean up duplicate interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -23,14 +23,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   },
 );
-
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
 
 export default api;
